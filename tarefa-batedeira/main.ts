98bcd42263f4af8e6645b3af7d2bf62709d5af39
eusouakau <karoltvbl@gmail.com>
2024-04-14 19:03:20 -0300

    refac: add camada de abstração e add batedor

--- tarefa-batedeira/main.ts
+++ tarefa-batedeira/main.ts
@@ -1,22 +1,43 @@
 
-type Batedor = "globo" | "leque" | "gancho";
-
-class BatedeiraPlanetaria {
-  constructor(public batedor: Batedor) {}
-
-  bater() {
-    if (this.batedor === "globo") {
-      console.log("Com esse batedor eu posso fazer sorvete!");
-    }
-
-    if (this.batedor === "leque") {
-      console.log("Com esse batedor eu posso fazer biscoito!");
-    }
-
-    if (this.batedor === "gancho") {
-      console.log("Com esse batedor eu posso fazer pão!");
-    }
-  }
+// type Batedor = "globo" | "leque" | "gancho" | "x";
+interface Batedor {
+    bater(): void
 }
-const batedeira = new BatedeiraPlanetaria("globo");
-console.log(batedeira.bater()); // Saída: Com esse batedor eu posso fazer pão!
+class BatedorGlobo implements Batedor {
+    bater(): void {
+        console.log("Com esse batedor eu posso fazer sorvete!");
+    }
+}
+
+class BatedorLeque implements Batedor {
+    bater(): void {
+        console.log("Com esse batedor eu posso fazer biscoito!");
+    }
+}
+
+class BatedorGancho implements Batedor {
+    bater(): void {
+        console.log("Com esse batedor eu posso fazer pão!");
+    }
+}
+
+class BatedorX implements Batedor {
+    bater(): void {
+        console.log("Com esse batedor eu posso fazer x!");
+    }
+}
+class BatedeiraPlanetaria {
+    acessorio: Batedor;
+
+    constructor(acessorio: Batedor) {
+      this.acessorio = acessorio;
+    }
+
+    bater() {
+      this.acessorio.bater();
+    }
+}
+const ganchoAcessorio = new BatedorGancho();
+
+const batedeira = new BatedeiraPlanetaria(ganchoAcessorio);
+batedeira.bater(); 
